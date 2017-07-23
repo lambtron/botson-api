fd5a70a9fc98fc4a65932d506dc7db1dc6ead046
Andy Jiang <andyjiang@gmail.com>
2017-07-23 12:41:15 -0700

    add logic for caching new accounts

--- index.js
+++ index.js
@@ -5,11 +5,9 @@
 
 require('dotenv').config()
 const { router, get, post } = require('microrouter')
-const db_account = require('./lib/db-account')
-const botometer = require('./lib/botometer')
-const twitter = require('./lib/twitter')
 const { send, json } = require('micro')
 const Raven = require('raven')
+const score = require('./lib/score')
 const Analytics = require('analytics-node')
 const analytics = new Analytics('aO23Wx83MZQnaPWyRvxffagdSm9I302w')
 Raven.config(process.env.SENTRY_SECRET_URL, { captureUnhandledRejections: true }).install();
@@ -34,24 +32,13 @@ const auth = (req, res) =>
 
 const get_score = async (req, res) => {
   if (!req.headers['x-twitter-oauth-token'] || !req.headers['x-twitter-oauth-secret']) send(res, 401, 'Twitter not authenticated')
+  if (req.headers['x-twitter-user-id']) analytics.track({ userId: req.headers['x-twitter-user-id'], event: 'Account Checked' })
   const auth = { oauth_token: req.headers['x-twitter-oauth-token'], oauth_token_secret: req.headers['x-twitter-oauth-secret'] }
   const body = await json(req)
   if (!body || !body.user_id || !body.screen_name) send(res, 400, 'Invalid user')
   body.user_id = '' + body.user_id
-  if (req.headers['x-twitter-user-id']) analytics.track({ userId: req.headers['x-twitter-user-id'], event: 'Account Checked' })
-  const account = await db_account.get(body.user_id)
-  let score = account
-  try {
-    if (!account) {
-      const tweets = await twitter.get_tweets(body.user_id, auth)
-      const mentions = await twitter.get_mentions(body.screen_name, auth)
-      const botometer_res = await botometer.check_account({ id: body.user_id, screen_name: body.screen_name }, tweets, mentions)
-      score = await db_account.upsert(body.user_id, { user_id: body.user_id, screen_name: body.screen_name, categories: botometer_res.categories, scores: botometer_res.scores })
-    }
-    send(res, 200, score)
-  } catch(e) {
-    send(res, 400, e)
-  }
+  let ret = await score.get(body, auth)
+  send(res, 200, ret)
 }
 
 /**
